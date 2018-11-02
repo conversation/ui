6270f8104cf2fc337724b0cd505fafd95b45294f
Josh Bassett <josh.bassett@gmail.com>
2018-11-03 07:18:03 +1100

    Refactor colour stories

--- src/Button/stories.jsx
+++ src/Button/stories.jsx
@@ -4,8 +4,8 @@ import { storiesOf } from '@storybook/react'
 
 import { Button, Grid } from '../index';
 
-const COLORS = ['default', 'primary', 'secondary']
-const SIZES = ['small', 'medium', 'large']
+const colors = ['default', 'primary', 'secondary']
+const sizes = ['small', 'medium', 'large']
 
 const GridDecorator = story => (
   <Grid container justify="center" spacing={16}>
@@ -16,28 +16,28 @@ const GridDecorator = story => (
 storiesOf('Buttons', module)
   .addDecorator(GridDecorator)
   .add('text', () => (
-    COLORS.map((color, index) =>
+    colors.map((color, index) =>
       <Grid item>
         <Button key={index} variant="text" color={color} onClick={action(color)}>{color}</Button>
       </Grid>
     )
   ))
   .add('contained', () => (
-    COLORS.map((color, index) =>
+    colors.map((color, index) =>
       <Grid item>
         <Button key={index} variant="contained" color={color} onClick={action(color)}>{color}</Button>
       </Grid>
     )
   ))
   .add('outlined', () => (
-    COLORS.map((color, index) =>
+    colors.map((color, index) =>
       <Grid item>
         <Button key={index} variant="outlined" color={color} onClick={action(color)}>{color}</Button>
       </Grid>
     )
   ))
   .add('sizes', () => (
-    SIZES.map((size, index) =>
+    sizes.map((size, index) =>
       <Grid item>
         <Button key={index} variant="contained" size={size} onClick={action(size)}>{size}</Button>
       </Grid>
--- src/Colour/stories.jsx
+++ src/Colour/stories.jsx
@@ -3,26 +3,22 @@ import { storiesOf } from '@storybook/react'
 
 import { Grid, Paper, Typography, withStyles } from '../index'
 
-const COLORS = ['primary', 'secondary', 'error']
+const colors = ['primary', 'secondary', 'error']
 
-const styles = theme => ({
+const styles = {
   root: {
     width: 120,
     paddingTop: 50,
     paddingBottom: 50
-  },
-  label: {
-    ...theme.typography.button,
-    textAlign: 'center'
   }
-})
+}
 
 const Swatch = ({ classes, color, theme }) => {
   const variant = theme.palette[color]
 
   return (
-    <Paper classes={{ root: classes.root }} style={{ backgroundColor: variant.main }}>
-      <Typography classes={{ root: classes.label }} style={{ color: variant.contrastText }}>
+    <Paper className={classes.root} style={{ backgroundColor: variant.main }}>
+      <Typography align="center" style={{ color: variant.contrastText }} variant="button">
         {color}
       </Typography>
     </Paper>
@@ -40,7 +36,7 @@ const GridDecorator = story => (
 storiesOf('Colors', module)
   .addDecorator(GridDecorator)
   .add('palette', () => (
-    COLORS.map((color, index) =>
+    colors.map((color, index) =>
       <Grid item>
         <ExampleSwatch key={index} color={color} />
       </Grid>
