b8a91b5945a89c47e8b18a447c416498ab0b03f1
Josh Bassett <josh.bassett@gmail.com>
2018-11-08 20:51:17 +1100

    Update package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as Grid } from '@material-ui/core/Grid';
+export { default as Grid } from '@material-ui/core/Grid'
 export { default as Paper } from '@material-ui/core/Paper'
 export { default as Typography } from '@material-ui/core/Typography'
 
