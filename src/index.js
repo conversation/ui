b00025ddf99df909229ca9779cb103db3464d298
Josh Bassett <josh.bassett@gmail.com>
2018-11-21 14:03:41 +1100

    Re-export Dialog components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+// Re-exported Material UI (MUI) components.
+//
+// Ideally, we shouldn't expose too much of the raw MUI API because it means
+// that our apps will be tightly coupled to an API we don't control. It's
+// better to wrap MUI components in our own wrapper, even if it doesn't add any
+// functionality.
+//
+// While we are actively developing this library, it does makes sense to give
+// ourselves a head start with a few MUI components.
+export { default as Dialog } from '@material-ui/core/Dialog'
+export { default as DialogActions } from '@material-ui/core/DialogActions'
+export { default as DialogContent } from '@material-ui/core/DialogContent'
+export { default as DialogTitle } from '@material-ui/core/DialogTitle'
 export { default as Grid } from '@material-ui/core/Grid'
 export { default as Paper } from '@material-ui/core/Paper'
 export { default as Typography } from '@material-ui/core/Typography'
 
+// Our API.
 export { ThemeProvider, defaultTheme, withStyles, withTheme } from './styles'
 export { default as Button } from './Button'
 export { default as Swatch } from './colours/Swatch'
