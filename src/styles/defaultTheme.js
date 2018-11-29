0d639b946674c5e1b57f94de6f22ad16e43ffda8
Josh Bassett <josh.bassett@gmail.com>
2018-11-29 15:59:05 +1100

    Don't capitalise button text

--- src/styles/defaultTheme.js
+++ src/styles/defaultTheme.js
@@ -34,7 +34,7 @@ export default function defaultTheme () {
 
       button: {
         fontFamily: 'Noto Sans',
-        textTransform: 'capitalize'
+        textTransform: 'none'
       },
 
       caption: {
