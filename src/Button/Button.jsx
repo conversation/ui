be7b80811ae3ad45728d9ce1e693ac6624f359d3
Josh Bassett <josh.bassett@gmail.com>
2018-12-04 11:09:09 +1100

    Add button prop types

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,13 +1,42 @@
 import MaterialButton from '@material-ui/core/Button'
+import PropTypes from 'prop-types'
 import React from 'react'
 
-/**
- * Overrides the Material Button component with some custom styles.
- */
+/** The `<Button>` component represents a clickable button. */
 const Button = ({ children, colour, ...other }) => (
   <MaterialButton color={colour} {...other}>
     {children}
   </MaterialButton>
 )
+
+Button.propTypes = {
+  /**
+   * The colour of the button.
+   *
+   * Primary buttons (e.g. save, yes, etc.) should have a higher visual
+   * empasis, while secondary buttons (e.g. delete, etc.) are given a different
+   * emphasis to indicate to the user that they perform an auxilary action.
+   */
+  colour: PropTypes.oneOf(['default', 'primary', 'secondary']),
+
+  /** If `true`, the button will be disabled. */
+  disabled: PropTypes.bool,
+
+  /** The callback function called when the button is clicked. */
+  onClick: PropTypes.func,
+
+  /** The size of the button. */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+
+  /** The variant of the button. */
+  variant: PropTypes.oneOf(['text', 'outlined', 'contained'])
+}
+
+Button.defaultProps = {
+  colour: 'default',
+  disabled: false,
+  size: 'medium',
+  variant: 'contained'
+}
 
 export default Button
