2a0cdab466cb500951fe790798093aa631f88195
Josh Bassett <josh.bassett@gmail.com>
2018-12-14 16:39:19 +1100

    Allow the storybook theme to be changed dynamically

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,8 +3,9 @@ import 'typeface-montserrat'
 import 'typeface-noto-sans'
 
 import React from 'react'
-import { ThemeProvider } from '../src'
+import { ThemeProvider, themes } from '../src'
 import { addDecorator, configure } from '@storybook/react'
+import { withKnobs, select } from '@storybook/addon-knobs';
 
 function loadStories () {
   importAll(require.context('../src', true, /.stories.jsx?$|stories\/index.jsx?$/))
@@ -18,12 +19,17 @@ function importAll (r) {
   }).forEach(r)
 }
 
-const CenterDecorator = story => (
-  <ThemeProvider>
-    {story()}
-  </ThemeProvider>
-)
+addDecorator(story => {
+  const name = select('Theme', Object.keys(themes), 'legacy')
+  const theme = themes[name]()
 
-addDecorator(CenterDecorator)
+  return (
+    <ThemeProvider theme={theme}>
+      {story()}
+    </ThemeProvider>
+  )
+})
+
+addDecorator(withKnobs)
 
 configure(loadStories, module)
--- src/styles/ThemeProvider.jsx
+++ src/styles/ThemeProvider.jsx
@@ -1,22 +1,42 @@
 import JssProvider from 'react-jss/lib/JssProvider'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import legacy from './themes/legacy'
 
-export default class ThemeProvider extends React.Component {
-  constructor (props) {
-    super(props)
-    this.theme = this.props.theme || legacy()
-  }
+const ThemeProvider = ({ children, sheetsManager, sheetsRegistry, theme, ...other }) => (
+  <JssProvider registry={sheetsRegistry}>
+    <MuiThemeProvider theme={theme} sheetsManager={sheetsManager} {...other} >
+      {children}
+    </MuiThemeProvider>
+  </JssProvider>
+)
 
-  render () {
-    return (
-      <JssProvider registry={this.props.sheetsRegistry}>
-        <MuiThemeProvider theme={this.theme} sheetsManager={this.props.sheetsManager}>
-          {this.props.children}
-        </MuiThemeProvider>
-      </JssProvider>
-    )
-  }
+ThemeProvider.propTypes = {
+  /**
+   * The child components to which the theme will be applied.
+   */
+  children: PropTypes.node.isRequired,
+
+  /**
+   * A `sheetsManager` is used to handle stylesheet injection in the page.
+   */
+  sheetsManager: PropTypes.object,
+
+  /**
+   * A `sheetsRegistry` is used to keep track of the stylesheet.
+   */
+  sheetsRegistry: PropTypes.object,
+
+  /**
+   * A theme object or function.
+   */
+  theme: PropTypes.oneOfType([PropTypes.object, PropTypes.func]).isRequired
 }
+
+ThemeProvider.defaultProps = {
+  theme: legacy()
+}
+
+export default ThemeProvider
