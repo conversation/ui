ba104ccf170329b5ea9ad2fb406f5cc75b77333c
Zoe Jazz <zoe.jazz@theconversation.com>
2019-04-08 13:49:20 +1000

    Update colour grid and variants

--- src/styles/stories/colours.jsx
+++ src/styles/stories/colours.jsx
@@ -3,15 +3,22 @@ import React from 'react'
 import { Swatch } from '../../index'
 import { GridLayout } from '../../util'
 
-const COLOURS = ['primary', 'secondary', 'error']
+const COLOURS = ['neutral', 'core', 'accent', 'positive', 'aware']
 
 export default () => (
   <GridLayout>
     {COLOURS.map((colour, index) =>
       <div key={index}>
-        <Swatch colour={colour} />
-        <Swatch colour={colour} variant='dark' />
-        <Swatch colour={colour} variant='light' />
+        {/* <Swatch colour={colour} /> */}
+        <Swatch colour={colour} variant='900' />
+        <Swatch colour={colour} variant='800' />
+        <Swatch colour={colour} variant='700' />
+        <Swatch colour={colour} variant='600' />
+        <Swatch colour={colour} variant='400' />
+        <Swatch colour={colour} variant='200' />
+        <Swatch colour={colour} variant='50' />
+        <Swatch colour={colour} variant='40' />
+        <Swatch colour={colour} variant='0' />
       </div>
     )}
   </GridLayout>
