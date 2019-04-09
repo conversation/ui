4193058e3264932e3782af18e1478b8a2d0f366e
nickbrowne <nickbrowne@users.noreply.github.com>
2019-04-09 16:35:39 +1000

    Order colours vertically and variants horizontally

--- src/styles/stories/colours.jsx
+++ src/styles/stories/colours.jsx
@@ -3,22 +3,16 @@ import React from 'react'
 import { Swatch } from '../../index'
 import { GridLayout } from '../../util'
 
-const COLOURS = ['neutral', 'core', 'accent', 'positive', 'aware']
+const COLOURS = ['primary', 'secondary', 'core']
+const VARIANTS = ['dark', 'main', 'light']
 
 export default () => (
   <GridLayout>
-    {COLOURS.map((colour, index) =>
+    {VARIANTS.map((variant, index) =>
       <div key={index}>
-        {/* <Swatch colour={colour} /> */}
-        <Swatch colour={colour} variant='900' />
-        <Swatch colour={colour} variant='800' />
-        <Swatch colour={colour} variant='700' />
-        <Swatch colour={colour} variant='600' />
-        <Swatch colour={colour} variant='400' />
-        <Swatch colour={colour} variant='200' />
-        <Swatch colour={colour} variant='50' />
-        <Swatch colour={colour} variant='40' />
-        <Swatch colour={colour} variant='0' />
+        {COLOURS.map((colour, index2) =>
+          <Swatch colour={colour} variant={variant} />
+        )}
       </div>
     )}
   </GridLayout>
