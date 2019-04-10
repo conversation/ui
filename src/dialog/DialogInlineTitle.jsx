887b20ab12b2c961476d3a1a254ce63fd7795d74
nickbrowne <nickbrowne@users.noreply.github.com>
2019-04-10 16:19:18 +1000

    Add DialogInlineTitle

---
+++ src/dialog/DialogInlineTitle.jsx
@@ -0,0 +1,27 @@
+import MaterialDialogTitle from '@material-ui/core/DialogTitle'
+import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+import Typography from '../Typography'
+
+const styles = theme => ({
+  root: {
+    color: theme.palette.primary.contrastText,
+    paddingTop: 0,
+    paddingBottom: 0
+  }
+})
+
+/**
+ * Overrides the Material DialogTitle component with some custom styles to
+ * make it slightly less prominent.
+ */
+const DialogInlineTitle = ({ children, ...other }) => (
+  <MaterialDialogTitle disableTypography {...other}>
+    <Typography color='inherit' variant='h6'>
+      {children}
+    </Typography>
+  </MaterialDialogTitle>
+)
+
+export default withStyles(styles)(DialogInlineTitle)
