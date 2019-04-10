5f62ca4e0884b46e44c6539fa5c122c96bad9691
nickbrowne <nickbrowne@users.noreply.github.com>
2019-04-10 17:07:59 +1000

    Map images to dummy module for jest
    This way it won't blow up trying to import binary data

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-  testMatch: ['<rootDir>/src/**/*.test.js?(x)']
+  testMatch: ['<rootDir>/src/**/*.test.js?(x)'],
+  moduleNameMapper: {
+    '\\.(jpg|png)$': '<rootDir>/test/dummy_module.js'
+  }
 }
