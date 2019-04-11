98ac8c357ef15c485280b829a0ac259563baa7cb
Josh Bassett <josh.bassett@gmail.com>
2019-04-11 11:14:07 +1000

    Rename dummy_module.js to dummy.png

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   testMatch: ['<rootDir>/src/**/*.test.js?(x)'],
   moduleNameMapper: {
-    '\\.(jpg|png)$': '<rootDir>/test/dummy_module.js'
+    '\\.(jpg|png)$': '<rootDir>/test/dummy.png'
   }
 }
