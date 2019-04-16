3edca96e80abe5b166eb26608267c3ede8dcc3ce
Josh Bassett <josh.bassett@gmail.com>
2019-04-16 10:41:49 +1000

    Mock GIF and SVG image assets

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   testMatch: ['<rootDir>/src/**/*.test.js?(x)'],
   moduleNameMapper: {
-    '\\.(jpg|png)$': '<rootDir>/test/blank.png'
+    '\\.(jpg|png|gif|svg)$': '<rootDir>/test/blank.png'
   }
 }
