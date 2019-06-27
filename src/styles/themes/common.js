e474ea123e85a6ffae453c97afa04d02cc23fb10
Mark Cipolla <mark@markcipolla.com>
2019-06-27 17:12:08 +1000

    Set default colours to neutral, extract typography and error colours
    This changes the default colour to `#555768`, and extracts shared
    typography and error colours to a separate shared file.

---
+++ src/styles/themes/common.js
@@ -0,0 +1,36 @@
+export const typography = {
+  // Enable v2 variants.
+  // https://material-ui.com/style/typography/#migration-to-typography-v2
+  useNextVariants: true,
+
+  fontFamily: 'Noto Sans',
+
+  h1: { fontFamily: 'Montserrat', fontWeight: 700 },
+  h2: { fontFamily: 'Montserrat', fontWeight: 700 },
+  h3: { fontFamily: 'Montserrat', fontWeight: 700 },
+  h4: { fontFamily: 'Montserrat', fontWeight: 600 },
+  h5: { fontFamily: 'Montserrat', fontWeight: 600 },
+  h6: { fontFamily: 'Montserrat', fontWeight: 700 },
+
+  body1: {
+    fontFamily: 'Libre Baskerville',
+    fontSize: '1rem',
+    lineHeight: '1.5'
+  },
+
+  button: {
+    letterSpacing: 0.25,
+    textTransform: 'none'
+  },
+
+  overline: {
+    letterSpacing: 0.25,
+    textTransform: 'none'
+  }
+}
+
+export const errorStateColours = {
+  palette: {
+    error: { main: '#990000', border: '#d8352a' }
+  }
+}
--- src/styles/themes/default.js
+++ src/styles/themes/default.js
@@ -1,51 +1,23 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import memoize from 'lodash/memoize'
+import merge from 'deepmerge'
+import { typography, errorStateColours } from './common'
 
 /**
  * This theme contains the styles which represent the future design of our
  * applications.
  */
 
-const theme = createMuiTheme({
-  palette: {
-    // mui colours
-    primary: { main: '#5499db' },
-    secondary: { main: '#f1f1f2' },
-    error: { main: '#990000', border: '#d8352a' },
-
-    // expanded colours
-    core: { main: '#D8352A' }
-  },
-  typography: {
-    // Enable v2 variants.
-    // https://material-ui.com/style/typography/#migration-to-typography-v2
-    useNextVariants: true,
-
-    fontFamily: 'Noto Sans',
-
-    h1: { fontFamily: 'Montserrat', fontWeight: 700 },
-    h2: { fontFamily: 'Montserrat', fontWeight: 700 },
-    h3: { fontFamily: 'Montserrat', fontWeight: 700 },
-    h4: { fontFamily: 'Montserrat', fontWeight: 600 },
-    h5: { fontFamily: 'Montserrat', fontWeight: 600 },
-    h6: { fontFamily: 'Montserrat', fontWeight: 700 },
-
-    body1: {
-      fontFamily: 'Libre Baskerville',
-      fontSize: '1rem',
-      lineHeight: '1.5'
-    },
-
-    button: {
-      letterSpacing: 0.25,
-      textTransform: 'none'
-    },
-
-    overline: {
-      letterSpacing: 0.25,
-      textTransform: 'none'
-    }
-  }
-})
+const theme = createMuiTheme(
+  merge(
+    {
+      palette: {
+      // mui colours
+        primary: { main: '#555768' },
+        secondary: { main: '#f1f1f2' }
+      },
+      typography
+    }, errorStateColours)
+)
 
 export default memoize(() => theme)
