737fa917630fcce55651dfbeca3e2373ed944a68
Mark Cipolla <mark@markcipolla.com>
2019-09-13 12:12:57 +1000

    Restructure the MessageTile examples to show a better range of uses
    Also, I've replicated a current TCAU `EndOfArticle` component.
    It used `ArticleDonationBanner` under the hood. The aim is to deprecate
    the 'donation'-related banners, in favour of more customisable/configurable
    components.

--- src/MessageTile/stories/index.jsx
+++ src/MessageTile/stories/index.jsx
@@ -1,11 +1,13 @@
 import { storiesOf } from '@storybook/react'
 import messageTile from './messageTile'
+import examples from './examples'
 import messageTileHeader from './messageTileHeader'
 import messageTileBody from './messageTileBody'
 import messageTileButton from './messageTileButton'
 
 storiesOf('MessageTile', module)
   .add('Overview', messageTile)
+  .add('Examples', examples)
   .add('MessageTileHeader', messageTileHeader)
   .add('MessageTileBody', messageTileBody)
   .add('MessageTileButton', messageTileButton)
--- src/MessageTile/stories/messageTile.jsx
+++ src/MessageTile/stories/messageTile.jsx
@@ -1,7 +1,6 @@
 import ComponentOverview from '../../ComponentOverview'
 import React from 'react'
 import { ThemeProvider } from '../../styles'
-import Typography from '../../Typography'
 import MessageTile, { MessageTile as UnwrappedMessageTile } from '../MessageTile'
 import MessageTileHeader from '../MessageTileHeader'
 import MessageTileBody from '../MessageTileBody'
@@ -12,13 +11,11 @@ import by from './by.svg'
 import cc from './cc.svg'
 import nd from './nd.svg'
 
-import accentTheme from '../../styles/themes/accent'
-import coreTheme from '../../styles/themes/core'
 import defaultTheme from '../../styles/themes/default'
 
 export default () => (
   <ComponentOverview heading='MessageTile' component={UnwrappedMessageTile}>
-    <div style={{ width: 220 }}>
+    <div style={{ marginTop: 20, width: 220 }}>
       <ThemeProvider theme={defaultTheme()}>
         <MessageTile>
           <MessageTileHeader>
@@ -37,54 +34,6 @@ export default () => (
           </MessageTileButton>
         </MessageTile>
       </ThemeProvider>
-
-      <Typography
-        style={{ clear: 'both', margin: '20px 0' }}
-        variant='h4'
-      >
-        Core Theme
-      </Typography>
-
-      <div style={{ width: 220 }}>
-        <ThemeProvider theme={coreTheme()}>
-          <MessageTile>
-            <MessageTileHeader>
-              Support The Conversation. Please donate.
-            </MessageTileHeader>
-
-            <MessageTileBody>
-              We provide clean information in a world infected with spin.
-            </MessageTileBody>
-
-            <MessageTileButton onClick={action('clicked')}>
-              Donate now
-            </MessageTileButton>
-          </MessageTile>
-        </ThemeProvider>
-      </div>
-
-      <Typography
-        style={{ clear: 'both', margin: '20px 0' }}
-        variant='h4'
-      >
-        Accent Theme
-      </Typography>
-
-      <div style={{ width: 400 }}>
-        <ThemeProvider theme={accentTheme()}>
-          <MessageTile>
-            <MessageTileHeader>
-              Want to follow The Conversation?
-            </MessageTileHeader>
-            <MessageTileBody>
-              Subscribe to our daily newsletter and get the latest analysis and commentary directly in your inbox.
-            </MessageTileBody>
-            <MessageTileButton onClick={action('clicked')}>
-              Subscribe now
-            </MessageTileButton>
-          </MessageTile>
-        </ThemeProvider>
-      </div>
     </div>
   </ComponentOverview>
 )
