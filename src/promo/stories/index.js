e249fda85e0bb4e98ecda4ed777bf0e52a94a840
Mark Cipolla <mark@markcipolla.com>
2019-09-13 11:23:16 +1000

    Move ThinBanner component to top-level in Storybook
    We decided as a group to remove the Promos storybook "folder", and
    instead move the promos we're keeping (Thin/Thick banner) to the top level.

--- src/promo/stories/index.js
+++ src/promo/stories/index.js
@@ -3,7 +3,6 @@ import { storiesOf } from '@storybook/react'
 import articleDonationBanner from './articleDonationBanner'
 import donationBanner from './donationBanner'
 import donationDialog from './donationDialog'
-import ThinBanner from './ThinBanner'
 import ThickBanner from './ThickBanner'
 import thinDonationBanner from './thinDonationBanner'
 
@@ -11,6 +10,5 @@ storiesOf('Promos', module)
   .add('ArticleDonationBanner', articleDonationBanner)
   .add('DonationBanner', donationBanner)
   .add('DonationDialog', donationDialog)
-  .add('ThinBanner', ThinBanner)
   .add('ThickBanner', ThickBanner)
   .add('ThinDonationBanner', thinDonationBanner)
