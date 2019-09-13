0d96e18ecc9b71a941bb0e3a57114cbbdb5bc534
Mark Cipolla <mark@markcipolla.com>
2019-09-13 11:31:01 +1000

    Move ThickBanner component to top-level in Storybook

--- src/promo/stories/index.js
+++ src/promo/stories/index.js
@@ -3,12 +3,10 @@ import { storiesOf } from '@storybook/react'
 import articleDonationBanner from './articleDonationBanner'
 import donationBanner from './donationBanner'
 import donationDialog from './donationDialog'
-import ThickBanner from './ThickBanner'
 import thinDonationBanner from './thinDonationBanner'
 
 storiesOf('Promos', module)
   .add('ArticleDonationBanner', articleDonationBanner)
   .add('DonationBanner', donationBanner)
   .add('DonationDialog', donationDialog)
-  .add('ThickBanner', ThickBanner)
   .add('ThinDonationBanner', thinDonationBanner)
