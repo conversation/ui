16ae47e2f619995e98ada6a1db926d0afbf97ce4
Mark Cipolla <mark@markcipolla.com>
2019-09-19 10:57:48 +1000

    Add bones of StickPopup component

---
+++ src/StickyPopup/StickyPopup.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const styles = theme => ({})
+
+/**
+ * The `<StickyPopup>` component informs users of something while still allowing the
+ * user to interact with the rest of the page.
+ */
+export const StickyPopup = ({}) => {
+  return (
+
+  )
+}
+
+StickyPopup.defaultProps = {
+}
+
+StickyPopup.propTypes = {
+}
+
+export default withStyles(styles)(StickyPopup)
