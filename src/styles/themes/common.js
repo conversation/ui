2239efdfc9adab47a464c639fdebd1bc54536444
Mark Cipolla <mark@markcipolla.com>
2019-09-23 11:34:20 +1000

    Update all base font sizes for Typography base variants
    MUI is mobile-first, so these styles appear for every device

--- src/styles/themes/common.js
+++ src/styles/themes/common.js
@@ -3,38 +3,80 @@ import core from '../palettes/core'
 export const typography = {
   fontFamily: 'Noto Sans',
 
-  h1: { fontFamily: 'Montserrat', fontWeight: 700 },
-  h2: { fontFamily: 'Montserrat', fontWeight: 700 },
-  h3: { fontFamily: 'Montserrat', fontWeight: 700 },
-  h4: { fontFamily: 'Montserrat', fontWeight: 600 },
-  h5: { fontFamily: 'Montserrat', fontWeight: 600 },
+  h1: {
+    fontFamily: 'Montserrat',
+    fontWeight: 700,
+    fontSize: 28,
+  },
+
+  h2: {
+    fontFamily: 'Montserrat',
+    fontWeight: 700,
+    fontSize: 26,
+  },
+
+  h3: {
+    fontFamily: 'Montserrat',
+    fontWeight: 700,
+    fontSize: 22,
+  },
+
+  h4: {
+    fontFamily: 'Montserrat',
+    fontWeight: 600,
+    fontSize: 20,
+  },
+
+  h5: {
+    fontFamily: 'Montserrat',
+    fontWeight: 600,
+    fontSize: 18,
+  },
+
   h6: {
     fontFamily: 'Montserrat',
     fontWeight: 700,
     lineHeight: 'normal',
-    letterSpacing: '0.25px'
+    letterSpacing: '0.25px',
+    fontSize: 16,
   },
 
   body1: {
     fontFamily: 'Libre Baskerville',
     fontSize: '1rem',
-    lineHeight: '1.5'
+    lineHeight: '1.5',
+    fontSize: 16
   },
 
   body2: {
     fontFamily: 'Noto Sans',
     fontSize: '0.875rem',
-    lineHeight: '1.5'
+    lineHeight: '1.5',
+    fontSize: 18
   },
 
   button: {
     letterSpacing: 0.25,
-    textTransform: 'none'
+    textTransform: 'none',
+    fontSize: 14
   },
 
   overline: {
     letterSpacing: 0.25,
-    textTransform: 'none'
+    textTransform: 'none',
+    fontSize: 14
+  },
+
+  caption: {
+    fontSize: 12
+  },
+
+  subtitle1: {
+    fontSize: 16
+  },
+
+  subtitle2: {
+    fontSize: 12
   }
 }
 
