76cf2396685da187a3164d9c6b3b47b6cec28b7f
Mark Cipolla <mark@markcipolla.com>
2019-09-24 13:12:56 +1000

    Move Typography font sizes to Typography component
    By placing them in the global styles, we're overriding any third-party
    component we want to use to force to use out font sizes.
    
    While this sounds a great idea, it means we were getting expectedly
    large text in our use of pickers (date / time / date + time).
    
    I've moved the font sizes (apart from Button) to the Typography component.

--- src/styles/themes/common.js
+++ src/styles/themes/common.js
@@ -8,69 +8,6 @@ export const typography = {
   fontFamily: 'Noto Sans',
   fontSize: 16,
 
-  h1: {
-    fontFamily: 'Montserrat',
-    fontWeight: 700,
-    fontSize: "1.75rem",
-    [theme.breakpoints.up('sm')]: {
-      fontSize: "2.75rem"
-    }
-  },
-
-  h2: {
-    fontFamily: 'Montserrat',
-    fontWeight: 700,
-    fontSize: "1.625rem",
-    [theme.breakpoints.up('sm')]: {
-      fontSize: "2rem"
-    }
-  },
-
-  h3: {
-    fontFamily: 'Montserrat',
-    fontWeight: 700,
-    fontSize: "1.375rem",
-  },
-
-  h4: {
-    fontFamily: 'Montserrat',
-    fontWeight: 600,
-    fontSize: "1.25rem",
-  },
-
-  h5: {
-    fontFamily: 'Montserrat',
-    fontWeight: 600,
-    fontSize: "1.125rem",
-  },
-
-  h6: {
-    fontFamily: 'Montserrat',
-    fontWeight: 700,
-    lineHeight: 'normal',
-    letterSpacing: '0.25px',
-    fontSize: "1rem",
-  },
-
-  body1: {
-    fontFamily: 'Libre Baskerville',
-    lineHeight: '1.5',
-    fontSize: "1rem",
-    [theme.breakpoints.up('sm')]: {
-      fontSize: "1.125rem"
-    }
-  },
-
-  body2: {
-    fontFamily: 'Noto Sans',
-    fontSize: '0.875rem',
-    lineHeight: '1.5',
-    fontSize: "1.125rem",
-    [theme.breakpoints.up('sm')]: {
-      fontSize: "1.5rem"
-    }
-  },
-
   button: {
     letterSpacing: 0.25,
     textTransform: 'none',
@@ -78,27 +15,6 @@ export const typography = {
     [theme.breakpoints.up('sm')]: {
       fontSize: "1rem"
     }
-  },
-
-  overline: {
-    letterSpacing: 0.25,
-    textTransform: 'none',
-    fontSize: "0.875rem",
-    [theme.breakpoints.up('sm')]: {
-      fontSize: "1rem"
-    }
-  },
-
-  caption: {
-    fontSize: "0.75rem"
-  },
-
-  subtitle1: {
-    fontSize: "1rem",
-  },
-
-  subtitle2: {
-    fontSize: "0.75rem"
   }
 }
 
