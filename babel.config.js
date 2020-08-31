dbe4e16d518f3c3863960d02d46e8a2776f1e3d0
nickbrowne <nickbrowne@users.noreply.github.com>
2020-08-31 16:36:43 +1000

    Add distinct CJS and ESM versions to dist/
    This leaves the ES imports intact for the ESM version of the library,
    which will allow webpack to tree shake unused modules.

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,15 @@
 module.exports = api => {
-  const env = api.cache(() => process.env.NODE_ENV)
-  const ignore = env === 'test' ? [] : [/test\.jsx?$/]
+  const nodeEnv = api.cache(() => process.env.NODE_ENV)
+  const ignore = nodeEnv === 'test' ? [] : [/test\.jsx?$/]
   const presets = ['@babel/preset-env', '@babel/preset-react']
   const plugins = ['@babel/plugin-proposal-class-properties']
+  const env = {
+    esm: {
+      presets: [
+        ['@babel/preset-env', { modules: false }], '@babel/preset-react'
+      ]
+    }
+  }
 
-  return { ignore, plugins, presets }
+  return { ignore, plugins, presets, env }
 }
