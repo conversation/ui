9550b150fcce5fbc2a2f6d001271157440bbef68
Tiago Amaro <tiagopadrela@gmail.com>
2021-09-08 14:10:23 -0300

    Delete the Autocomplete component
    Autocomplete component wasn't used, as we can use any third
    party libraries with our existent text field components to
    create autocomplete feature of our own.

--- src/Autocomplete/stories/overview.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import ComponentOverview from '../../ComponentOverview'
-import { Autocomplete } from '../Autocomplete'
-
-export default () => <ComponentOverview component={Autocomplete} />
